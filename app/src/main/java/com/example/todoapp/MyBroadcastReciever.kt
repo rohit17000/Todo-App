2f781254d2ab27a437a98ddc927c4c0076e9bfc7
Rohit Thakur <rt3278978@gmail.com>
2021-07-05 19:45:41 +0530

    initial commit

---
+++ app/src/main/java/com/example/todoapp/MyBroadcastReciever.kt
@@ -0,0 +1,14 @@
+package com.example.todoapp
+
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.media.MediaPlayer
+
+class MyBroadcastReciever: BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+
+    }
+
+}
